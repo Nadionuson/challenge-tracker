312e2272e648d70064289fecab3b94b2eb3a9a92
Oliveira <nunooliveira@deloitte.pt>
2026-04-07 16:15:18 +0100

    chore: add iron-session and Jest with React Testing Library

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: { jsx: 'react-jsx' } }],
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+}
+
+export default config
