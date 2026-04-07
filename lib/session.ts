7b28a3c2d36cd0b38a73b7e77ed7f619fd27fc3f
Oliveira <nunooliveira@deloitte.pt>
2026-04-07 16:38:54 +0100

    feat: iron-session config

---
+++ lib/session.ts
@@ -0,0 +1,14 @@
+import { SessionOptions } from 'iron-session'
+
+export interface SessionData {
+  authenticated: boolean
+}
+
+export const sessionOptions: SessionOptions = {
+  password: process.env.SESSION_SECRET as string,
+  cookieName: 'challenge-tracker-session',
+  cookieOptions: {
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 60 * 60 * 24 * 30, // 30 days
+  },
+}
